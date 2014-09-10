cde5d5214defa2cc27aa4323805b72ae4322fd31
Peter Benoit <peterbenoit@gmail.com>
2014-09-09 20:00:23 -0400

    seems to "randomly" not work... hmm.

--- RoboGrunt.bat
+++ RoboGrunt.bat
@@ -1,1 +1,1 @@
-rmdir "c:\build_output\3.0" /S/Q && grunt && robocopy "c:\TemplatePackage\3.0" "\\APD-V-NCHM-VSS1\idk1\tmp\3.0" /S /XO /FFT /XD ".git" ".svn" ".gitattributes" ".gitignore"
+rmdir "c:\build_output\3.0" /S/Q && grunt && robocopy "c:\TemplatePackage\3.0" "\\APD-V-NCHM-VSS1\idk1\TemplatePackage\3.0" /S /XO /FFT /XD ".git" ".svn" ".gitattributes" ".gitignore"
